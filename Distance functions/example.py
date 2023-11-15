705dd13be1a906b695b42dbb5c97bfb2965b575d
Leonid Savtchenko <leonid.savtchenko@ucl.ac.uk>
2023-11-15 11:14:18 +0000

    Update example.py

--- Distance functions/example.py
+++ Distance functions/example.py
@@ -1,7 +1,6 @@
 
-from math import *
+import numpy as np
 
 def myDistFunc(x):
-    y = x/10 + 50*sin(2*pi*x/200) * tanh(x/100)
+    y = 1 + np.sign(x / 10 + 50 * np.sin(2 * np.pi * x / 200) * np.tanh(x / 100))
     return y
-    
